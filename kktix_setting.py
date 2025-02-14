4d9e9ce39dc4d4783f1225d93f8a96f4aca1b0e6
ann.huang <ann.huang@healthconn.com>
2025-02-15 03:19:53 +0800

    更改設定檔

--- kktix_setting.py
+++ kktix_setting.py
@@ -4,13 +4,13 @@ PASSWORD = ''
 # SELL_URL = 'https://kktix.com/events/watn2025/registrations/new'
 SELL_URL = 'https://kktix.com/events/leemujinconcert2025-01/registrations/new'
 # SELL_URL = 'https://kktix.com/events/d116d7c2/registrations/new'
-# SELL_URL = 'https://kktix.com/events/yooyeonseokhk2025/registrations/new'
+SELL_URL = 'https://kktix.com/events/umfrd08/registrations/new'
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
 AREA_LOCATOR = '.display-table-row'
 VIEWPORT = {'width': 1440, 'height': 1080}
 TICKET_NUMBER = '1'
 
-EXCLUDED_AREAS = ['身障','票種','票價','紅2B','紫2B']
+EXCLUDED_AREAS = ['身障','票種','票價','紅2B','紫2B','3樓']
 # PREFERRED_AREAS = ['A5區', 'A6區','A7區','A8區','A9區', 'A10區','A11區','A12區']
-PREFERRED_AREAS = ['VIP']
+PREFERRED_AREAS = ['特']
 ONLY_PREFERRED_AREAS = False
